0d86c75e8f7d5b631ead3b58561e1ab32bb6c75b
Danny Williams <mmpreply@yahoo.com>
2012-10-05 09:30:21 -0400

    mmp_ast project - In this commit I made deploy2 which prepares the server for asterisk realtime and JAACT. JAACT is something I started writing 2 years ago that I need to finish. My Managed PBX might be back soon

---
+++ deploy2.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+#######################################
+# Written by Danny Williams JR. 
+# My Managed PBX, Cause we love Linux and Open source we write quality stuff!
+#
+# This setup script is licenced under the GPLv2. You make changes I better
+# get a copy!. mmpreply@yahoo.com.
+#
+# You might have to fix the paths. But I wrote this for Ubuntu Server.
+# So your experience may vary on say Fedora or CentOS.
+#######################################
+
+#!/bin/bash
+
+# Download vicidial and asterisk.
+wget http://downloads.sourceforge.net/project/astguiclient/astguiclient_2.4rc2.zip
+wget http://downloads.asterisk.org/pub/telephony/asterisk/old-releases/asterisk-1.6.2.16.1.tar.gz
+http://downloads.asterisk.org/pub/telephony/asterisk/old-releases/asterisk-addons-1.6.2.4.tar.gz
+
+# Ubuntu setup
+apt-get install \
+	imagemagick \
+	apache2 \
+	apache2-utils \
+	libapache2-mod-perl2 \
+	libapache2-mod-php5 \
+	php5 \
+	php5-dev \
+	php5-cli \
+	php5-mysql \
+	php5-imagick \
+	xml-core \
+	xsltproc \
+	php-pear \
+	libgd-tools \
+        sox
+
+# Work in the Perl stuff
+echo "install YAML" | perl -MCPAN -e 'shell'
+perl -MCPAN -e 'install Moose'
+perl -MCPAN -e 'install XML::Writer'
+perl -MCPAN -e 'install DBI'
+perl -MCPAN -e 'force install DBD::mysql'
+perl -MCPAN -e 'install XML::Simple'
+perl -MCPAN -e 'install XML::Generator'
+perl -MCPAN -e 'install XML::Generator::DBI'
+perl -MCPAN -e 'install Asterisk'
+perl -MCPAN -e 'install XML::Handler::YAWriter'
+perl -MCPAN -e 'install Digest::MD5'
+perl -MCPAN -e 'install Cache::Memcached::Fast'
+echo "install Catalyst" | perl -MCPAN -e 'shell'
+echo "install Catalyst::Devel" | perl -MCPAN -e 'shell'
+echo "install Catalyst::Helper::Model::DBIC::Schema" | perl -MCPAN -e 'shell'
+echo "install Catalyst::Helper::View::TT" | perl -MCPAN -e 'shell'
+echo "install Catalyst::Plugin::Redirect" | perl -MCPAN -e 'shell'
+echo "install Catalyst::Engine::Apache" | perl -MCPAN -e 'shell'
+echo "install Catalyst::Plugin::Authentication" | perl -MCPAN -e 'shell'
+echo "install Catalyst::Plugin::Session" | perl -MCPAN -e 'shell'
+echo "install Catalyst::Plugin::Session::Store::FastMmap" | perl -MCPAN -e 'shell'
+echo "install Catalyst::Plugin::Session::State::Cookie" | perl -MCPAN -e 'shell'
+echo "install Catalyst::Plugin::StackTrace" | perl -MCPAN -e 'shell'
+echo "install Catalyst::Authentication::Store::DBIx::Class" | perl -MCPAN -e 'shell'
+echo "install Moose" | perl -MCPAN -e 'shell'
+echo "install Text::Template" | perl -MCPAN -e 'shell'
+echo "install DBI" | perl -MCPAN -e 'shell'
+echo "install DBD::mysql" | perl -MCPAN -e 'shell'
+echo "install DateTime::Format::MySQL" | perl -MCPAN -e 'shell'
+echo "install Asterisk::AGI" | perl -MCPAN -e 'shell'
+echo "install Asterisk::Manager" | perl -MCPAN -e 'shell'
+echo "install Spreadsheet::WriteExcel" | perl -MCPAN -e 'shell'
+
+echo "Edit /etc/my.cnf and set query_cache_size = 32M"
