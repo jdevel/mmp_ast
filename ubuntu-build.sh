a1dd5dfd42ffc026e4231a8dac2afcfb639fc37a
Danny Williams <mmpreply@yahoo.com>
2012-10-15 20:09:17 -0400

    mmp_ast - ubuntu 8.04 does not come with screen by default. added to ubuntu build scipt

--- ubuntu-build.sh
+++ ubuntu-build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 apt-get update
-apt-get install build-essential zip ccache doxygen linux-headers-2.6.24-26-server
+apt-get install build-essential zip ccache doxygen linux-headers-2.6.24-26-server screen
