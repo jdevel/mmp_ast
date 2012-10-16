478182a202118706a3b6b2154236d95fda2cdf3a
Danny Williams <mmpreply@yahoo.com>
2012-10-15 20:01:54 -0400

    mmp_ast update - added a script that creates the mysql tables for vicidial. Need to clean this up and create these scripts as step 1 -> x update part 2 fix for backticks around asterisk word in create database

--- mysql-stuff.sh
+++ mysql-stuff.sh
@@ -6,3 +6,8 @@ mysql -u root -p -e "GRANT SELECT,INSERT,UPDATE,DELETE,LOCK TABLES on asterisk.*
 mysql -u root -p -e "GRANT RELOAD ON *.* TO cron@'%'" 
 mysql -u root -p -e "GRANT RELOAD ON *.* TO cron@localhost" 
 mysql -u root -p -e "flush privileges"
+
+echo "cd astguiclient_x.xrc2/extras and run the followings"
+echo "mysql -u root -p asterisk < MySQL_AST_CREATE_tables.sql && mysql -u root -p asterisk < sip-iax_phones.sql && mysql -u root -p asterisk < first_server_install.sql"
+echo ""
+echo "Have a nice day >:)"
