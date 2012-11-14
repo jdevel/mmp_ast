b4b8235461ad21f755af469f8fabc5d6f4ebfba8
Danny Williams <mmpreply@yahoo.com>
2012-11-14 18:44:25 -0500

    mmp_ast - applied a fix to the missing crc_ccitt for the ztdummy driver. applied fix to ubuntu-build.sh

--- ubuntu-build.sh
+++ ubuntu-build.sh
@@ -2,3 +2,5 @@
 apt-get update
 apt-get install build-essential zip ccache doxygen linux-headers-2.6.24-26-server screen unzip
 apt-get clean
+echo "crc_ccitt" >> /etc/modules
+modprobe -a crc_ccitt
