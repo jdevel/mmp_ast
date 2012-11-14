a9c9134f4505dc31adbc0e639116f7b898fc8d64
Danny Williams <mmpreply@yahoo.com>
2012-11-14 12:36:00 -0500

    mmp_ast - more cleanup to the automation process and added more notes in the README to make my life easier

--- ubuntu-build.sh
+++ ubuntu-build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 apt-get update
-apt-get install build-essential zip ccache doxygen linux-headers-2.6.24-26-server screen
+apt-get install build-essential zip ccache doxygen linux-headers-2.6.24-26-server screen unzip
+apt-get clean
