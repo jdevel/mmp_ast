88aa71bdfcaf53a7713c8ecc88d0e86596d14368
Danny Williams <mmpreply@yahoo.com>
2012-12-13 13:53:54 -0500

    mmp_ast update to ubuntu-build.sh. Needed to add curl.

--- ubuntu-build.sh
+++ ubuntu-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get update
-apt-get install build-essential zip ccache doxygen linux-headers-2.6.24-26-server screen unzip
+apt-get install build-essential zip ccache doxygen linux-headers-2.6.24-26-server screen unzip curl
 apt-get clean
 echo "crc_ccitt" >> /etc/modules
 modprobe -a crc_ccitt
